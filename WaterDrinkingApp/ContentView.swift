4916a11324e6e205279ce9d5a99c4f95e2596115
Dario Mintzer <dmintzer75@hotmail.com>
2022-03-04 16:10:12 +0100

    First Commit

--- WaterDrinkingApp/ContentView.swift
+++ WaterDrinkingApp/ContentView.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color("BrandLightBlue")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+   
+                Text("Drink one glass of Water!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                
+                Image("DrinkWaterImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+            }
+            
+                
+        }
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
