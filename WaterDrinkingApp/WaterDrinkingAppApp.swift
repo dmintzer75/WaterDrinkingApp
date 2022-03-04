43212022c840db09b12ae6577aa18d8560fd43a2
Dario Mintzer <dmintzer75@hotmail.com>
2022-03-04 15:15:06 +0100

    Initial Commit

---
+++ WaterDrinkingApp/WaterDrinkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterDrinkingAppApp.swift
+//  WaterDrinkingApp
+//
+//  Created by Dario Mintzer on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WaterDrinkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
